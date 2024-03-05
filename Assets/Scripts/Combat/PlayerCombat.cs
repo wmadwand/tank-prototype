a9fdbd9a795ad62474f526b21b3e04493d7a7139
Vasilii Manoilov <wmadwand@gmail.com>
2024-03-05 17:39:03 +0100

    Make sure the laser only lasts for about 0.1 seconds after being shot and then dissapears

--- Assets/Scripts/Combat/PlayerCombat.cs
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -12,17 +12,17 @@ namespace TestingTask.Combat
         private float _nextShotTime;
 
         private const float SHOT_COOLDOWN = 1f;
-        
+
         public PlayerCombat()
         {
             _playerController = Object.FindObjectOfType<PlayerController>();
         }
-        
+
         public void Shoot(ITargetable target)
         {
             if (CanShoot() == false)
                 return;
-            
+
             var damageTarget = target.gameObject.GetComponent<IDamageable>();
             if (damageTarget == null)
                 return;
@@ -31,12 +31,23 @@ namespace TestingTask.Combat
             damageTarget.TakeDamage(50);
             _nextShotTime = Time.time + SHOT_COOLDOWN;
         }
-        
+
+        //TODO: Use TrailRenderer
         private void RenderLaser(Vector3 targetPosition)
+        {
+            _playerController.StartCoroutine(RenderLaserRoutine(targetPosition));
+        }
+
+        private IEnumerator RenderLaserRoutine(Vector3 targetPosition)
         {
             var laserLine = _playerController.LaserLine;
             laserLine.SetPosition(0, laserLine.transform.position);
             laserLine.SetPosition(1, targetPosition);
+            laserLine.enabled = true;
+
+            yield return new WaitForSeconds(0.1f);
+
+            laserLine.enabled = false;
         }
 
         private bool CanShoot()
